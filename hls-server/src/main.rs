e044226bb8304d528430e8d4237e2de2f7580169
madebysid <me@madebysid.com>
2020-08-29 05:53:18 +0200

    Get a basic stream working

--- hls-server/src/main.rs
+++ hls-server/src/main.rs
@@ -1,4 +1,6 @@
-use std::io::Read;
+use flate2::write::GzEncoder;
+use flate2::Compression;
+use std::io::prelude::*;
 
 #[async_std::main]
 async fn main() -> Result<(), std::io::Error> {
@@ -11,9 +13,32 @@ async fn main() -> Result<(), std::io::Error> {
             .body(
                 "
         <html>
+            <head>
+                <meta charset=\"UTF-8\">
+            </head>
             <body>
-                <video width=\"640\" height:\"480\" controls src=\"./index.m3u8\">
-                </video>
+                <script src=\"//cdn.jsdelivr.net/npm/hls.js@latest\"></script>
+                <video id=\"video\" width=\"1280\" height=\"720\" controls></video>
+                <script>
+                    if (Hls.isSupported()) {
+                        var video = document.getElementById('video');
+                        var hls = new Hls({ debug: true });
+                        hls.loadSource(`https://0b4ec16ca43a.ngrok.io/bbb.m3u8`);
+                        // hls.loadSource(`https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8`)
+                        hls.attachMedia(video);
+                        hls.on(Hls.Events.MEDIA_ATTACHED, function() {
+                            video.play();
+                        });
+                        hls.on(Hls.Events.ERROR, function (event, data) {
+                            console.log(`Error in stream`, event, data)
+                        });
+                    } else if (video.canPlayType('application/vnd.apple.mpegurl')) {
+                        video.src = 'https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8';
+                        video.addEventListener('canplay',function() {
+                            video.play();
+                        });
+                    }
+                </script>
             </body>
         </html>
         ",
@@ -22,37 +47,43 @@ async fn main() -> Result<(), std::io::Error> {
             .build())
     });
 
-    app.at("/:file").get(|req: tide::Request<()>| async move {
-        let filename: String = req.param("file").unwrap_or("index.m3u8".to_string());
-        let path =
-            std::path::Path::new("/home/ozark/Video/The Big Lebowski (1998)/hls").join(&filename);
+    app.at("/bbb.m3u8").get(|_| async move {
+        let path = std::path::Path::new("/home/ozark/Video/hls/index.m3u8");
 
         let mut file = std::fs::File::open(path)?;
         let mut bytes = Vec::new();
         file.read_to_end(&mut bytes).unwrap();
 
-        let parsed = m3u8_rs::parse_playlist_res(&bytes);
+        let response = tide::Response::builder(200)
+            .body(bytes)
+            .header("Access-Control-Allow-Origin", "*")
+            .header("Access-Control-Expose-Headers", "Content-Length")
+            .header("Access-Control-Allow-Headers", "Range")
+            .header("Content-Type", "application/vnd.apple.mpegurl");
+        Ok(response.build())
+    });
 
-        match parsed {
-            Ok(m3u8_rs::playlist::Playlist::MasterPlaylist(_)) => unimplemented!(),
-            Ok(m3u8_rs::playlist::Playlist::MediaPlaylist(p)) => {
-                println!("{:?}", p.version);
-                // p.segments.iter().for
-            }
-            Err(e) => println!("Error: {:?}", e),
-        }
+    app.at("/:file").get(|req: tide::Request<()>| async move {
+        let filename: String = req.param("file")?;
+        let path = std::path::Path::new("/home/ozark/Video/hls").join(&filename);
+
+        let mut file = std::fs::File::open(path)?;
+        let mut bytes = Vec::new();
+        file.read_to_end(&mut bytes)?;
+
+        let mut encoder = GzEncoder::new(Vec::new(), Compression::default());
+        encoder.write_all(&bytes)?;
+        let bytes = encoder.finish()?;
 
         let response = tide::Response::builder(200)
             .body(bytes)
-            .header("Content-Type", {
-                if filename.ends_with(".m3u8") {
-                    "vnd.apple.mpegURL"
-                } else if filename.ends_with(".ts") {
-                    "video/MP2T"
-                } else {
-                    "video/mp4"
-                }
-            });
+            .header("Access-Control-Allow-Origin", "*")
+            .header("Content-Encoding", "gzip")
+            .header("Access-Control-Expose-Headers", "Content-Length")
+            .header("Access-Control-Allow-Headers", "Range")
+            .header("Content-Type", "application/octet-stream");
+
+        println!("Responding");
         Ok(response.build())
     });
 
