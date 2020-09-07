149b3d6aa02bbf5cde54e86712df46e6cfd344c0
madebysid <me@madebysid.com>
2020-09-07 22:46:21 +0200

    Add a lowercase filter for handlebars

--- src/app_state.rs
+++ src/app_state.rs
@@ -1,4 +1,5 @@
-use handlebars::Handlebars;
+use anyhow::*;
+use handlebars::{Context, Handlebars, Helper, JsonRender, Output, RenderContext, RenderError};
 use std::sync::Arc;
 
 #[derive(Debug, Clone)]
@@ -6,13 +7,29 @@ pub struct AppState {
     pub templates: Arc<Handlebars<'static>>,
 }
 
-// lazy_static this
+fn lowercase(
+    h: &Helper,
+    _: &Handlebars,
+    _: &Context,
+    _: &mut RenderContext,
+    out: &mut dyn Output,
+) -> Result<(), RenderError> {
+    let param = h
+        .param(0)
+        .ok_or(RenderError::new("Param 0 is required for format helper."))?;
+    let param = format!("{}", param.value().render());
+    out.write(&param.to_lowercase())?;
+    Ok(())
+}
+
 impl AppState {
     pub fn new() -> Self {
         let mut handlebars = Handlebars::new();
         handlebars
             .register_templates_directory(".hbs", "src/templates/")
             .unwrap();
+
+        handlebars.register_helper("lowercase", Box::new(lowercase));
 
         Self {
             templates: Arc::new(handlebars),
