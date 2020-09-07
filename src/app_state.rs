f87bd36c373156eaa319a8b4011fedcef819e854
madebysid <me@madebysid.com>
2020-09-08 00:14:10 +0200

    File size format

--- src/app_state.rs
+++ src/app_state.rs
@@ -1,4 +1,5 @@
 use anyhow::*;
+use bytesize::ByteSize;
 use handlebars::{Context, Handlebars, Helper, JsonRender, Output, RenderContext, RenderError};
 use std::sync::Arc;
 
@@ -14,12 +15,37 @@ fn lowercase(
     _: &mut RenderContext,
     out: &mut dyn Output,
 ) -> Result<(), RenderError> {
-    let param = h
-        .param(0)
-        .ok_or(RenderError::new("Param 0 is required for format helper."))?;
+    let param = h.param(0).ok_or(RenderError::new(
+        "Param 0 is required for `lowercase` helper.",
+    ))?;
     let param = format!("{}", param.value().render());
     out.write(&param.to_lowercase())?;
     Ok(())
+}
+
+fn filesize(
+    h: &Helper,
+    _: &Handlebars,
+    _: &Context,
+    _: &mut RenderContext,
+    out: &mut dyn Output,
+) -> Result<(), RenderError> {
+    let param = h.param(0).ok_or(RenderError::new(
+        "Param 0 is required for `filesize` helper.",
+    ))?;
+    if param.value().is_number() {
+        let size = param.value().as_u64().ok_or(RenderError::new(
+            "Param `filesize` helper must be a positive number",
+        ))?;
+
+        if size > 0 {
+            out.write(&ByteSize::b(size).to_string())?;
+        }
+
+        Ok(())
+    } else {
+        Err(RenderError::new("Param `filesize` helper must be a number"))
+    }
 }
 
 impl AppState {
@@ -30,6 +56,7 @@ impl AppState {
             .unwrap();
 
         handlebars.register_helper("lowercase", Box::new(lowercase));
+        handlebars.register_helper("filesize", Box::new(filesize));
 
         Self {
             templates: Arc::new(handlebars),
