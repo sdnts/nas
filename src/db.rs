c2d1e9d0ea43b2b4fd229d487b5797c3ea0d0035
madebysid <me@madebysid.com>
2020-09-12 16:15:35 +0200

    NASDB restructuring

--- src/db.rs
+++ src/db.rs
@@ -1,67 +1,44 @@
 use anyhow::*;
 use rusqlite::{params, Connection};
-use serde::Serialize;
-use sha2::{Digest, Sha512};
 use std::path::Path;
-use std::str;
 
-#[derive(Clone, Debug, Serialize)]
-pub struct User {
-    id: u8,
-    username: String,
-    password_hash: String,
-}
-
-pub struct NASDB {
-    connection: Connection,
-}
+#[derive(Debug)]
+pub struct NASDB(pub Connection);
 
 impl NASDB {
     pub fn new() -> Result<Self> {
         let db_file = Path::new(&crate::CONFIG.fs_root).join("db.sqlite");
         let connection = Connection::open(db_file)?;
+        Ok(Self(connection))
+    }
 
-        connection.execute(
-            "CREATE TABLE IF NOT EXISTS User (
-                  id              INTEGER PRIMARY KEY AUTOINCREMENT,
-                  username        TEXT NOT NULL,
-                  password_hash   TEXT NOT NULL
-                  )",
-            params![],
-        )?;
-
-        Ok(Self { connection })
+    pub fn connection(&self) -> &Connection {
+        &self.0
     }
 }
 
 impl NASDB {
-    pub fn login(&self, username: &str, password: &str) -> Result<User> {
-        let mut hasher = Sha512::new();
-        hasher.update(password);
-        let password_hash = hasher.finalize();
-        let password_hash = password_hash.as_slice().to_vec();
-        let password_hash = hex::encode(&password_hash);
+    pub fn init() -> Result<()> {
+        let db = NASDB::new()?;
+        let connection = db.0;
 
-        let mut stmt = self.connection.prepare(
-            "SELECT id,username,password_hash FROM User
-                WHERE username = ?1 AND password_hash = ?2",
+        connection.execute(
+            "CREATE TABLE IF NOT EXISTS Users (
+                id              INTEGER PRIMARY KEY AUTOINCREMENT,
+                username        TEXT NOT NULL,
+                password_hash   TEXT NOT NULL
+            )",
+            params![],
         )?;
 
-        let users: Vec<Result<User, rusqlite::Error>> = stmt
-            .query_map(&[username, &password_hash], |row| {
-                Ok(User {
-                    id: row.get(0)?,
-                    username: row.get(1)?,
-                    password_hash: row.get(2)?,
-                })
-            })?
-            .collect();
+        connection.execute(
+            "CREATE TABLE IF NOT EXISTS Sessions (
+                id TEXT PRIMARY KEY,
+                value TEXT UNIQUE
+            )",
+            params![],
+        )?;
 
-        let user = users[0]
-            .as_ref()
-            .map_err(|_| anyhow!("[db::login] Invalid credentials"))?
-            .to_owned();
-
-        Ok(user)
+        Ok(())
     }
 }
