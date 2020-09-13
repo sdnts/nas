b2b14609f56fb2139685205cbaaade430d321f71
madebysid <me@madebysid.com>
2020-09-13 18:42:02 +0200

    auth.get to actix-web

--- src/auth/get.rs
+++ src/auth/get.rs
@@ -1,7 +1,27 @@
-use actix_web::{Responder, Result};
+use actix_web::{web, HttpResponse, Responder, Result};
 
+use crate::app_state::AppState;
 use crate::error::NASError;
+use crate::templates::AuthPageParams;
 
-pub async fn get() -> Result<impl Responder> {
-    Ok(format!("/auth get"))
+pub async fn get(
+    path: web::Path<String>,
+    app_state: web::Data<AppState>,
+) -> Result<impl Responder> {
+    let templates = &app_state.templates;
+
+    let response_body = templates
+        .render(
+            "auth",
+            &AuthPageParams {
+                title: "/auth".to_string(),
+                hostname: "0zark".to_string(),
+                message: "".to_string(),
+            },
+        )
+        .map_err(|_| NASError::TemplateRenderError { template: "auth" })?;
+
+    Ok(HttpResponse::Ok()
+        .body(response_body)
+        .with_header("Content-Type", "text/html;charset=utf-8"))
 }
