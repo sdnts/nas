0d931300842c4e46676449062e3f6d9a5dbcbc6e
madebysid <me@madebysid.com>
2020-09-14 00:25:40 +0200

    Handle error in strip_trailing_char util

--- src/utils.rs
+++ src/utils.rs
@@ -1,6 +1,10 @@
 pub fn strip_trailing_char(string: String) -> String {
     let mut s = string.chars().rev();
-    s.next().unwrap();
-    let s = s.rev();
-    s.collect::<String>()
+
+    if let Some(_) = s.next() {
+        let s = s.rev();
+        s.collect::<String>()
+    } else {
+        "".to_string()
+    }
 }
